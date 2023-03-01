701ebe1d8e63e86180c35c50c7dce6f9f07a4906
nebjamin <jonb1@newpaltz.edu>
2023-03-01 18:09:11 -0500

    Update Navbar

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -5,83 +5,54 @@
 </script>
 
 <template>
-  <nav class="navbar is-dark is-spaced">
-      <div class="container">
-      <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
-          <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="22" height="22" />
+<nav class="navbar is-dark-blue is-link is-spaced has-shadow">
+    <div class="navbar-brand">
+        <a class="navbar-item" href="#">
+            <img src="https://bulma.io/images/bulma-logo-white.png" width="112" height="28">
         </a>
-        <div class="navbar-burger">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </div>
-    
-      <div class="navbar-menu">
+
+        <a class="navbar-burger">
+
+        </a>
+    </div>
+    <div class="navbar-menu">
         <div class="navbar-start">
-
-          <RouterLink to="/" class="navbar-item">Home</RouterLink>
-          <RouterLink to="/about" class="navbar-item">About</RouterLink>
-
-          <div class="navbar-item has-dropdown is-hoverable">
-            <a class="navbar-link" href="https://bulma.io/documentation/overview/start/">
-              Docs
+            <a class="navbar-item">
+                Home
             </a>
-            <div class="navbar-dropdown">
-              <a class="navbar-item" href="https://bulma.io/documentation/overview/start/">
-                Overview
-              </a>
-              <a class="navbar-item" href="https://bulma.io/documentation/overview/modifiers/">
-                Modifiers
-              </a>
-              <a class="navbar-item" href="https://bulma.io/documentation/columns/basics/">
-                Columns
-              </a>
-              <a class="navbar-item" href="https://bulma.io/documentation/layout/container/">
-                Layout
-              </a>
-              <a class="navbar-item" href="https://bulma.io/documentation/form/general/">
-                Form
-              </a>
-              <hr class="navbar-divider">
-              <a class="navbar-item" href="https://bulma.io/documentation/elements/box/">
-                Elements
-              </a>
-              <a class="navbar-item is-active" href="https://bulma.io/documentation/components/breadcrumb/">
-                Components
-              </a>
+            <div class="navbar-item has-dropdown is-hoverable">
+                <a class ="navbar-link">
+                    My Statistics
+                </a>
+
+                <div class="navbar-dropdown">
+                    <a href="" class="navbar-item">
+                        Cardio
+                    </a>
+                    <a href="" class="navbar-item">
+                        Weights
+                    </a>
+                </div>
             </div>
-          </div>
+            <a class="navbar-item">
+                My Friends
+            </a>
         </div>
-    
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="field is-grouped">
-              <p class="control">
-                <a class="bd-tw-button button is-info" data-social-network="Twitter" data-social-action="tweet" data-social-target="https://bulma.io" target="_blank" href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&amp;hashtags=bulmaio&amp;url=https://bulma.io&amp;via=jgthms">
-                  <span class="icon">
-                    <i class="fab fa-twitter"></i>
-                  </span>
-                  <span>
-                    Tweet
-                  </span>
-                </a>
-              </p>
-              <p class="control">
-                <a class="button is-light" href="https://github.com/jgthms/bulma/releases/download/0.9.3/bulma-0.9.3.zip">
-                  <span class="icon">
-                    <i class="fas fa-download"></i>
-                  </span>
-                  <span>Download</span>
-                </a>
-              </p>
-            </div>
-          </div>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-danger">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-danger">
+            Log in
+          </a>
         </div>
       </div>
-  </div>
-    </nav>
+    </div>
+  </nav>
 </template>
 
 <style scoped></style>
